aec30a7623844a4dfb1665000478a39774d09489
cpzh <maseratipp@gmail.com>
2018-06-30 08:48:39 +0800

    test COMMIT

---
+++ douby-source-code/src/main/java/com/douby/dubbocode/Protocol$Adaptive.java
@@ -0,0 +1,89 @@
+package com.douby.dubbocode;
+
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+
+/**
+ *                _ooOoo_
+ *                o8888888o
+ *                88" . "88
+ *                (| -_- |)
+ *                O\ = /O
+ *              ____/`---'\____
+ *            .   ' \\| |// `.
+ *              / \\||| : |||// \
+ *             / _||||| -:- |||||- \
+ *              | | \\\ - /// | |
+ *              | \_| ''\---/'' | |
+ *            \ .-\__ `-` ___/-. /
+ *            ___`. .' /--.--\ `. . __
+ *          ."" '< `.___\_<|>_/___.' >'"".
+ *         | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ *          \ \ `-. \_ __\ /__ _/ .-` / /
+ * ======`-.____`-.___\_____/___.-`____.-'======
+ *                `=---='
+ * .............................................
+ *      佛祖镇楼                  BUG辟易
+ * 佛曰:
+ *       写字楼里写字间，写字间里程序员；
+ *       程序人员写程序，又拿程序换酒钱。
+ *       酒醒只在网上坐，酒醉还来网下眠；
+ *       酒醉酒醒日复日，网上网下年复年。
+ *       但愿老死电脑间，不愿鞠躬老板前；
+ *       奔驰宝马贵者趣，公交自行程序员。
+ *       别人笑我忒疯癫，我笑自己命太贱；
+ *       不见满街漂亮妹，哪个归得程序员？
+ *
+ * @Author: cpzh
+ * @Date: 2018/6/28 20:47
+ * TODO: Protocol的一个适配器 createAdaptiveExtensionClassCode()生成字节码
+ */
+public class Protocol$Adaptive implements com.alibaba.dubbo.rpc.Protocol {
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("method public abstract void com.alibaba.dubbo.rpc.Protocol.destroy() of interface com.alibaba.dubbo.rpc.Protocol is not adaptive method!");
+    }
+
+    @Override
+    public int getDefaultPort() {
+        throw new UnsupportedOperationException("method public abstract int com.alibaba.dubbo.rpc.Protocol.getDefaultPort() of interface com.alibaba.dubbo.rpc.Protocol is not adaptive method!");
+    }
+
+    /**
+     * 应用服务
+     * @param arg0
+     * @param arg1
+     * @return
+     * @throws com.alibaba.dubbo.rpc.RpcException
+     */
+    @Override
+    public com.alibaba.dubbo.rpc.Invoker refer(java.lang.Class arg0, com.alibaba.dubbo.common.URL arg1) throws com.alibaba.dubbo.rpc.RpcException {
+        if (arg1 == null) throw new IllegalArgumentException("url == null");
+        com.alibaba.dubbo.common.URL url = arg1;
+        //如果不存在 就默认是dubbo协议
+        String extName = (url.getProtocol() == null ? "dubbo" : url.getProtocol());
+        if (extName == null)
+            throw new IllegalStateException("Fail to get extension(com.alibaba.dubbo.rpc.Protocol) name from url(" + url.toString() + ") use keys([protocol])");
+        com.alibaba.dubbo.rpc.Protocol extension = (com.alibaba.dubbo.rpc.Protocol) ExtensionLoader.getExtensionLoader(com.alibaba.dubbo.rpc.Protocol.class).getExtension(extName);
+        return extension.refer(arg0, arg1);
+    }
+
+    /**
+     * 发布服务
+     * @param arg0
+     * @return
+     * @throws com.alibaba.dubbo.rpc.RpcException
+     */
+    @Override
+    public com.alibaba.dubbo.rpc.Exporter export(com.alibaba.dubbo.rpc.Invoker arg0) throws com.alibaba.dubbo.rpc.RpcException {
+        if (arg0 == null) throw new IllegalArgumentException("com.alibaba.dubbo.rpc.Invoker argument == null");
+        if (arg0.getUrl() == null)
+            throw new IllegalArgumentException("com.alibaba.dubbo.rpc.Invoker argument getUrl() == null");
+        com.alibaba.dubbo.common.URL url = arg0.getUrl();
+        String extName = (url.getProtocol() == null ? "dubbo" : url.getProtocol());
+        if (extName == null)
+            throw new IllegalStateException("Fail to get extension(com.alibaba.dubbo.rpc.Protocol) name from url(" + url.toString() + ") use keys([protocol])");
+        com.alibaba.dubbo.rpc.Protocol extension = (com.alibaba.dubbo.rpc.Protocol) ExtensionLoader.getExtensionLoader(com.alibaba.dubbo.rpc.Protocol.class).getExtension(extName);
+        return extension.export(arg0);
+    }
+}
